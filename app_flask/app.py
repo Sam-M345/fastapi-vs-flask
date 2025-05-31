ef03a20d5b309cbf8ba6d4bee99cfde9b7f52c58
Sam Mirazi <sasan345@gmail.com>
2025-05-30 22:22:22 -0700

    app_flask   app.py auto launches browser and 3000

--- app_flask/app.py
+++ app_flask/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, Response
 import time
+import webbrowser
 
 app = Flask(__name__)
 
@@ -10,4 +11,11 @@ def home():
     return Response(html, mimetype="text/html")
 
 if __name__ == "__main__":
-    app.run() 
+    host = "127.0.0.1"
+    port = 3000
+    url = f"http://{host}:{port}/"
+    
+    # Open the URL in a new browser tab
+    webbrowser.open_new_tab(url)
+    
+    app.run(host=host, port=port) 
